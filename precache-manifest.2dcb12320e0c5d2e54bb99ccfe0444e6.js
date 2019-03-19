02ac635665f368c2e3486365bc1da84884911ef4
infl4me <hellpl4y@gmail.com>
2019-03-20 03:17:12 +0500

    Updates

---
+++ precache-manifest.2dcb12320e0c5d2e54bb99ccfe0444e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79d63be4180111765c10",
+    "url": "/react-todo-app/static/css/main.9e8fdb55.chunk.css"
+  },
+  {
+    "revision": "79d63be4180111765c10",
+    "url": "/react-todo-app/static/js/main.3f421029.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  },
+  {
+    "revision": "ae0b72b89335021e0997",
+    "url": "/react-todo-app/static/js/2.4952e05c.chunk.js"
+  },
+  {
+    "revision": "665f372e3de92ecb7e81d81222eaad35",
+    "url": "/react-todo-app/index.html"
+  }
+];
